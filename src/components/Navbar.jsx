9204f82770b81843a3d876614f7f723ffbe0929c
AnKub <ankub3088@gmail.com>
2024-09-18 14:23:40 +0300

    dosearch

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -22,7 +22,7 @@ export default function Navbar({ onSearch }) {
             <nav className="flex justify-between bg-slate-200 shadow-lg shadow-pink-600/40  rounded-3xl py-4">
                 <div>
                     <img src={logo} alt="Logo" className="inline-block ml-10 mb-2 mr-5 w-6 h-6" />
-                    <p className="inline-block text-2xl">{`Arts & Culture`}</p>
+                    <p className="inline-block ml-5 text-2xl">{`Arts & Culture`}</p>
                 </div>
                 <div className="bg-white rounded-3xl py-1 px-2 mr-5 ">
                     <Search className="inline-block mr-1 text-slate-500" />
